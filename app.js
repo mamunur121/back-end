21f88f5eb07f7843d3ef417ddf921c95f3735c7d
Md mamunur Rashid <mamun.ruet.ete@gmail.com>
2019-05-15 09:07:06 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// print numbers from 100 to 0
+for(let i = 100; i>= 0; i--){
+   // console.log(i);
+
+   // divisible by 3
+    if(i % 3 === 0) {
+        console.log("three");
+    }
+    // divisible by 5
+    else if (i % 5 === 0) {
+        console.log("five");
+    }
+}
